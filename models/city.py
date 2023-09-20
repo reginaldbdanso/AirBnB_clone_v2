279292dfb2957cbb51e8a5147d2e9b4647545d6c
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-20 21:44:44 +0000

    Updated imports

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
