51792f34350931ef1653c9636598d2eec63a9cc0
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-20 22:52:04 +0000

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,12 @@
--- Create the hbnb_dev_db database if it does not already exist
+-- Create the hbnb_test_db database if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create a new user called hbnb_dev with the password hbnb_dev_pwd
+-- Create a new user called hbnb_test with the password hbnb_test_pwd
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
--- Grant select privilege on the performance_schema database to the hbnb_dev user
+-- Grant select privilege on the performance_schema database to the hbnb_test user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
