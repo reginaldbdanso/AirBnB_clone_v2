c2f6cc5216bda979fa1ee14d2da224a945c548cd
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-20 22:44:27 +0000

    Updated setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,6 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
+FLUSH PRIVILEGES;
 -- Grant select privilege on the performance_schema database to the hbnb_dev user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
