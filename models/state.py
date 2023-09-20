d542cf2fb595a7e1caf70f75eeb01a3407d6d48a
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-20 21:33:46 +0000

    Updated models/state.py

--- models/state.py
+++ models/state.py
@@ -3,6 +3,24 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(
+        String(128),
+        nullable=False
+        )
+    cities = relationship(
+        "City",
+        backref="state",
+        cascade="all, delete"
+        )
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances with state_id equals to the current State.id"""
+        list_cities = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                list_cities.append(city)
+        return list_cities
