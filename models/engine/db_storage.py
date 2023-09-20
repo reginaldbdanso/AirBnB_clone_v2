b03eddfe0a3805e6b71b87a7fa47ebd7813a2c06
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-20 21:33:37 +0000

    Added models/engine/db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""This module is a db_storage engine """
+import os
+
+
+class DBStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Instantiation of DBStorage class"""
+        from sqlalchemy import create_engine
+        from sqlalchemy.orm import sessionmaker, scoped_session
+        from models.base_model import Base
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                HBNB_MYSQL_USER,
+                HBNB_MYSQL_PWD,
+                HBNB_MYSQL_HOST,
+                HBNB_MYSQL_DB
+                ),
+                pool_pre_ping=True
+                )
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = scoped_session(Session)
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        if cls is None:
+            objs = self.__session.query(User).all()
+            objs += self.__session.query(State).all()
+            objs += self.__session.query(City).all()
+            objs += self.__session.query(Amenity).all()
+            objs += self.__session.query(Place).all()
+            objs += self.__session.query(Review).all()
+        else:
+            objs = self.__session.query(classes[cls]).all()
+        new_dict = {}
+        for obj in objs:
+            key = obj.__class__.__name__ + '.' + obj.id
+            new_dict[key] = obj
+        return new_dict
+
+    def new(self, obj):
+        """
+        Adds new object to storage dictionary
+        (current database session)
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        Saves storage dictionary(commit changes to 
+        current database session)
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Loads storage dictionary from database"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
