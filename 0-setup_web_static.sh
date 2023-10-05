149d64f5876c1555746b6ae8a14116d5c28d8c63
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-10-05 20:32:17 +0000

    My first commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+
+# Install Nginx if it's not already installed
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+
+# Create the test and shared directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file for testing
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of the web_static folder
+of your AirBnB Clone repo, using the function do_pack.
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        # Create the folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Create the file name
+        now = datetime.now()
+        file_name = f"web_static_{now.year}{now.month}{now.day}{now.hour}{
+            now.minute}{now.second}.tgz"
+
+        # Create the archive
+        local("tar -cvzf versions/{file_name} web_static")
+
+        # Return the path to the archive
+        return f"versions/{file_name}"
+
+    except Exception as e:
+        return None
